2daae5a290e02a4dfe72dea226629e38d9146815
stefziv02 <stefanzivanic02skolski@gmail.com>
2024-06-19 12:22:04 +0200

    dataset folderi, 80/20 priblizno, ostale skripte iskopirane

---
+++ Teams/smeće_plast_čep/2_gen_labels.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+cfg = 'HSV_Thresholds.cfg.yaml'
+labeler = 'OpenCV/HSV/build/HSV_Labeler'
+images_dir = 'dataset/images/'
+labels_dir = 'dataset/gen_labels/'
+
+
+
+import os
+import glob
+import subprocess
+import shutil
+from os.path import *
+
+def run_cmd(cmd):
+	r = subprocess.run(cmd.split())
+	if r.returncode != 0:
+		error('failed cmd:', cmd)
+
+
+for subdir in ['train', 'val', 'test', 'trash']:
+	images_subdir = join(images_dir, subdir)
+	labels_subdir = join(labels_dir, subdir)
+	os.makedirs(labels_subdir, exist_ok = True)
+	for img in glob.glob(join(images_subdir, '*.jpg')):
+		b = basename(img)
+		c, e = splitext(b)
+		label = join(labels_subdir, c + ".txt")
+		run_cmd(f'{labeler} {cfg} {img} {label}')
+
+
