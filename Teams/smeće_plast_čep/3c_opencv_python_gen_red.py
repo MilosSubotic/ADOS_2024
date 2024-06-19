87796191c60bc38a3076ed7724283d74d41c9a89
stefziv02 <stefanzivanic02skolski@gmail.com>
2024-06-19 14:30:33 +0200

    hsv label za crvenu boju test

---
+++ Teams/smeće_plast_čep/3c_opencv_python_gen_red.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+# Učitavanje slike
+image = cv2.imread('/home/stefziv/Documents/ADOS/ADOS_2024_FORK/Teams/smeće_plast_čep/dataset/images/train/IMG_20240618_111935644_result.jpg')
+
+# Konverzija slike iz BGR u HSV prostor boja
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# Definisanje HSV opsega za ciljne boje (primer za crvenu boju)
+lower_red = np.array([159, 50, 70])
+upper_red = np.array([180, 255, 255])
+
+# Kreiranje maske za crvenu boju
+mask = cv2.inRange(hsv_image, lower_red, upper_red)
+
+# Primena morfoloških operacija (dilatacija i erozija)
+kernel = np.ones((5, 5), np.uint8)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+# Etiketiranje objekata
+num_labels, labels_im = cv2.connectedComponents(mask)
+
+# Normalizacija etiketa za prikaz
+labels_im = cv2.normalize(labels_im, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
+# Prikaz rezultata
+cv2.imshow('Original Image', image)
+cv2.imshow('Mask', mask)
+cv2.imshow('Labeled Image', labels_im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
