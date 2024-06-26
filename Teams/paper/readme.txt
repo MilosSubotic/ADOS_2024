58adb141f6651532f5ca305b59fc4425f26c9d99
mihailodikanovic <miki.dikanovic02@gmail.com>
2024-06-26 08:49:11 +0200

    jun 26 08:49

--- Teams/paper/readme.txt
+++ Teams/paper/readme.txt
@@ -1,7 +1,7 @@
 /cfg     - contains configuration files for the model and HSV labeler
 
 /dataset - contains /images and /labels split into /test, /train, /val
-           contains /countours where you can see labeled bounding boxes on each image for debbuging purposes
+           contains /countours where you can see bounding boxes on each image for debbuging purposes
            
 /runs    - contains model's training and prediction files
 
--- Teams/paper/tasks/2_generate_labels (hsv).py
+++ Teams/paper/tasks/2_generate_labels (hsv).py
@@ -3,21 +3,22 @@ import numpy as np
 import yaml
 import os
 
-# Function to read configuration file
+MIN_AREA_THRESHOLD = 2500
+
+
 def read_config(config_file):
     with open(config_file, 'r') as file:
         config = yaml.safe_load(file)
     return config['classes']
 
-# Function to process images
 def process_images(input_dir, output_dir, classes):
     # List subdirectories
     subdirs = os.listdir(input_dir)  # subdirs = [test, train, val]
 
-    # Proccess every subdirectory
+    # Proccess each subdirectory
     for subdir in subdirs:
-        sub_input_dir = os.path.join(input_dir, subdir)  # images/ + subdir
-        sub_output_dir = os.path.join(output_dir, subdir)  # labels/ + subdir
+        sub_input_dir = os.path.join(input_dir, subdir)            # images/ + subdir
+        sub_output_dir = os.path.join(output_dir, subdir)          # labels/ + subdir
         contours_dir = os.path.join('./dataset/contours', subdir)  # countours/ + subdir
         
         os.makedirs(sub_output_dir, exist_ok=True)  # Create subdirectory in output_dir if not exists
@@ -26,7 +27,7 @@ def process_images(input_dir, output_dir, classes):
         # Process each image in the subdirectory
         for image_name in os.listdir(sub_input_dir):
             if image_name.endswith(('.jpg', '.jpeg', '.png')):
-                # Read the image
+                # Read image
                 image_path = os.path.join(sub_input_dir, image_name)
                 image = cv2.imread(image_path)
                 if image is None:
@@ -35,16 +36,16 @@ def process_images(input_dir, output_dir, classes):
                 # Extract image dimensions
                 image_height, image_width, _ = image.shape  
 
-                # Convert the image to HSV color space
+                # Convert image to HSV color space
                 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-                # List to store all bounding boxes for detected objects
+                # List to store all bounding boxes
                 all_bounding_boxes = []
 
                 # Process each class defined in the configuration
                 for class_config in classes:
 
-                    # Define HSV range for the current class
+                    # Define HSV range for current class
                     h_start = class_config['H']['start']
                     h_stop = class_config['H']['stop']
 
@@ -54,63 +55,64 @@ def process_images(input_dir, output_dir, classes):
                     v_start = class_config['V']['start']
                     v_stop = class_config['V']['stop']
 
-                    # Make mask with HSV parameters
+                    # Mask image with HSV range
                     mask = cv2.inRange(hsv_image, 
                                            np.array([h_start, s_start, v_start]),
                                            np.array([h_stop, s_stop, v_stop]))
 
-                    # Find contours in the mask
+                    # Detect contours in masked image
                     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                    # Process each contour for the current class
+                    # Process each contour for current class
                     for contour in contours:
                         # Filter contours based on area
                         area = cv2.contourArea(contour)
-                        if area < 2500:  # Filter small, irrelevant objects
+                        if area < MIN_AREA_THRESHOLD:  # Filter small, irrelevant objects
                             continue
 
-                        # Get bounding box values
+                        # Get bounding box coordinates
                         x, y, w, h = cv2.boundingRect(contour)
 
-                        # Calculate normalized values (scale to [0,1] range)
+                        # Calculate normalized coordinates (scale to [0,1] range)
                         x_center = (x + w / 2) / image_width
                         y_center = (y + h / 2) / image_height
                         width = w / image_width
                         height = h / image_height
 
-                        # Store bounding box information
+                        # Store bounding box coordinates
                         all_bounding_boxes.append((class_config['class_id'], x_center, y_center, width, height))
 
-                        # Draw rectangle(bounding box) on the image for visualization
+                        # Draw rectangle(bounding box) on image for visualization
                         cv2.rectangle(image, (x, y), (x + w, y + h), class_config['bb_color'], 2)
                 
-                # Export images with bounding boxes for debug
+                # Export current image with bounding boxes for debug
                 output_image_path = os.path.join(contours_dir, image_name)
                 cv2.imwrite(output_image_path, image)
 				
-                # Write labels for every image in current subdirectory
+                # Write labels for current image
                 output_file_path = os.path.join(sub_output_dir, os.path.splitext(image_name)[0] + '.txt')
                 with open(output_file_path, 'w') as file:
                     for bbox_info in all_bounding_boxes:
                         class_id, x_center, y_center, width, height = bbox_info
                         file.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
 
-                # Write classes.txt file for the current subdirectory
-                classes_txt_path = os.path.join(sub_output_dir, 'classes.txt')
-                with open(classes_txt_path, 'w') as file:
-                    for class_config in classes:
-                        class_name = class_config['class_name']
-                        file.write(f"{class_name}\n")
+        # Write classes.txt file for current subdirectory
+        classes_txt_path = os.path.join(sub_output_dir, 'classes.txt')
+        with open(classes_txt_path, 'w') as file:
+            for class_config in classes:
+                class_name = class_config['class_name']
+                file.write(f"{class_name}\n")
 
     print("Labeling complete.")
 
-# Read configuration file
+
+# Read HSV configuration
 config_file = './cfg/HSV_Thresholds.cfg.yaml'
 classes = read_config(config_file)
 
-# Input and output directories
+# Set paths
 input_dir = './dataset/images'
 output_dir = './dataset/labels'
 
-# Process images in subdirectories of input_dir
+# Do labeling
 process_images(input_dir, output_dir, classes)
